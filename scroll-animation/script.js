728f4268827b168a7e3c2050c8deb38e1af598d9
Sohaib <sohaib.b.development@gmail.com>
2024-09-05 20:51:17 +0330

    fail to do project, not enough knowledge

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,7 +1,17 @@
 const originalContent = document.querySelector(".original");
 const mainPage = document.querySelector("section");
+const content = document.querySelectorAll(".original");
 let scroll = 0;
 let lastScroll = 0;
 let isLoading = false;
 
-document.addEventListener("scroll", () => {});
+document.addEventListener("scroll", () => {
+  scroll = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let pageHeight = mainPage.offsetHeight;
+  if (scroll + windowHeight <= pageHeight && !isLoading) {
+    content.forEach((e) => {
+      e.classList.add("slide-in");
+    });
+  }
+});
