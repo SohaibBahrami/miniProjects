c9c6addcae1726eda552bd386febf9d40737d57d
Sohaib <sohaib.b.development@gmail.com>
2024-09-06 11:55:09 +0330

    change positions to be more responsive and less snappy

--- split-landing-page/script.js
+++ split-landing-page/script.js
@@ -1,24 +1,24 @@
 const psSide = document.querySelector("#ps");
 const xboxSide = document.querySelector("#xbox");
-const psText = document.querySelector("div div");
-const xboxText = document.querySelector("xbox-div");
+const psText = document.querySelector("#ps-div div");
+const xboxText = document.querySelector("#xbox-div div");
 psSide.addEventListener("mouseover", () => {
-  psSide.style.transform = "translateX(-25vw)";
-  psText.style.transform = "translateX(-150px)";
-  xboxSide.style.transform = "translateX(75vw)";
+  psSide.style.transform = "translateX(20vw)";
+  xboxSide.style.transform = "translateX(20vw)";
+  psText.style.transform = "translateX(50px)";
 });
 psSide.addEventListener("mouseout", () => {
-  psSide.style.transform = "translateX(-50vw)";
+  psSide.style.transform = "translateX(0vw)";
+  xboxSide.style.transform = "translateX(0vw)";
   psText.style.transform = "translateX(0)";
-  xboxSide.style.transform = "translateX(50vw)";
 });
 xboxSide.addEventListener("mouseover", () => {
-  psSide.style.transform = "translateX(-75vw)";
-  xboxSide.style.transform = "translateX(25vw)";
-  xboxText.style.transform = "translateX(-150px)";
+  psSide.style.transform = "translateX(-20vw)";
+  xboxSide.style.transform = "translateX(-20vw)";
+  xboxText.style.transform = "translateX(-50px)";
 });
 xboxSide.addEventListener("mouseout", () => {
-  psSide.style.transform = "translateX(-50vw)";
-  xboxSide.style.transform = "translateX(50vw)";
+  psSide.style.transform = "translateX(0vw)";
+  xboxSide.style.transform = "translateX(0vw)";
   xboxText.style.transform = "translateX(0)";
 });
