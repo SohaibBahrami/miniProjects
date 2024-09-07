0ed13e7523357dc9aeb72ed9db2da35e1adca5ee
Sohaib <sohaib.b.development@gmail.com>
2024-09-07 13:44:12 +0330

    set the animation for the labels

--- form-wave/script.js
+++ form-wave/script.js
@@ -1,19 +1,36 @@
-const letters = document.querySelectorAll("label span");
+const emailLetters = document.querySelectorAll("#email-letters span");
+const passwordLetters = document.querySelectorAll("#password-letters span");
 const inputs = document.querySelectorAll("input");
 let currentIndex = 0;
 
-inputs.forEach((e) => {
-  e.addEventListener("click", moveUp);
+inputs[0].addEventListener("click", () => {
+  resetAnimation();
+  currentIndex = 0;
+  moveUpEmail();
+});
+inputs[1].addEventListener("click", () => {
+  resetAnimation();
+  currentIndex = 0;
+  moveUpPassword();
+});
+document.addEventListener("click", (event) => {
+  if (!event.target.closest("input")) {
+    resetAnimation();
+  }
 });
 
-function moveUp() {
-  if (currentIndex < letters.length) {
-    letters[currentIndex].style.transform = "translateY(225%)";
-    letters[currentIndex].style.color = "lightblue";
-    currentIndex++;
-    setTimeout(moveUp, 200);
-  }
+function moveUpEmail() {
+  emailLetters[currentIndex].classList.add("moved");
+  currentIndex++;
+  setTimeout(moveUpEmail, 50);
 }
-// function moveBack() {
-//   labels.forEach((e) => {});
-// }
+function moveUpPassword() {
+  passwordLetters[currentIndex].classList.add("moved");
+  currentIndex++;
+  setTimeout(moveUpPassword, 50);
+}
+function resetAnimation() {
+  emailLetters.forEach((letter) => letter.classList.remove("moved"));
+  passwordLetters.forEach((letter) => letter.classList.remove("moved"));
+  currentIndex = 0;
+}
