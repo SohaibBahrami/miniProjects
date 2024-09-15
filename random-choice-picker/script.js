bf9ffb016a1770d44d463c931d8e7aa021257ca5
Sohaib <sohaib.b.development@gmail.com>
2024-09-15 16:09:48 +0330

    update js to handle comma-separated input with split()

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -1,21 +1,28 @@
 const choiceInput = document.querySelector("#choice-input");
 const choiceContainer = document.querySelector("#choice-container");
+let count = -1;
 
-const choice = document.createElement("span");
-choice.classList.add("choices");
-choiceContainer.appendChild(choice);
+separate();
+const choiceObject = document.getElementsByClassName("choices-style");
 
-choiceInput.addEventListener("input", () => {
-  if (choiceInput.value.includes(",")) {
+choiceInput.addEventListener("keydown", (event) => {
+  if (event.key === ",") {
     separate();
   } else {
-    choice.textContent = choiceInput.value;
+    choiceObject[count].style.display = "inline";
+    const input = choiceInput.value
+      .split(",")
+      .filter((tag) => tag.trim() !== "")
+      .map((tag) => tag.trim());
+    console.log(choiceObject);
+    choiceObject[count].textContent = input[count];
   }
 });
 
 function separate() {
   const choice = document.createElement("span");
-  choice.classList.add("choices");
-  choice.textContent = choiceInput.value;
+  choice.classList.add("choices-style");
   choiceContainer.appendChild(choice);
+  choice.style.display = "none";
+  count++;
 }
